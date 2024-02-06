35e48857f9e7f954eb3d32e929c633ef8278dd3f
Evaly Adam <152657385+Evalyadam@users.noreply.github.com>
2024-02-06 10:35:05 +0200

    Initial Commit
    added files

---
+++ Test.js
@@ -0,0 +1,5 @@
+function MyFunction() {
+    document.getElementById("ILoveJavascript").innerHTML = 'Yay! I made myself cookies!';
+    window.alert("Would you like the paragraph to make cookies?");
+    console.log("User made cookies for parahraph! what a nice guy!");
+}
